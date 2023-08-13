8ecf033f8a70b7147b32e3cdc065e78ca97b8088
fakoredeDamilola <dfakorede432@stu.ui.edu.ng>
2023-08-13 20:25:54 +0100

    fixed error

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import "./App.css";
 import Header from "./components/Header";
 import LocalHighlight from "./components/LocalHighlight";
-import LocalityRadar from "./components/localityRadar";
 import LocalForecast from "./components/LocalForecast";
+import LocalityRadar from "./components/LocalityRadar";
 function App() {
   return (
     <>
